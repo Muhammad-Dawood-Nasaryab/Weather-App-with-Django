2b1499df43e12f77852edd894cfea7b8902ddd30
Macs <macs@Hassams-MacBook-Pro.local>
2024-10-07 18:46:16 +0300

    initial commit

---
+++ main/views.py
@@ -0,0 +1,46 @@
+import urllib.request 
+import json
+from django.shortcuts import render 
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST["city"]
+
+        try:
+            source = urllib.request.urlopen(
+                f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=e93a9abb570ba80b0d2eae7d5aeeef13"
+            ).read()
+            data = json.loads(source)
+
+            name = data["name"]
+            temperature = data["main"]["temp"]
+            condition = data["weather"][0]["main"]
+            description = data["weather"][0]["description"]
+            humidity = data["main"]["humidity"]
+
+            clData = {
+                "name": name,
+                "temperature": temperature - 273.15,
+                "condition": condition,
+                "description": description,
+                "humidity": humidity,
+            }
+        except urllib.error.URLError:
+            clData = {
+                "error": "City not found"
+            }
+        except KeyError:
+            clData = {
+                "error": "Invalid city name"
+            }
+        except Exception as e:
+            clData = {
+                "error": str(e)
+            }
+
+    else:
+        clData = {}
+
+    return render(request, "main/index.html", clData)
